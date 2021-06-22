b8520a6fc8fc7253e6c4038d7b8a37ff0bde233f
jomefavourite <jfjomefavourite@gmail.com>
2021-06-22 22:10:37 +0100

    intersection observer
    Signed-off-by: jomefavourite <jfjomefavourite@gmail.com>

--- public/main.js
+++ public/main.js
@@ -9,6 +9,12 @@ function selectAll(name) {
 const hamburger = select('.hamburger');
 const nav = select('.nav');
 const navLists = selectAll('.nav__list__item a');
+const features = document.querySelectorAll('.feature');
+
+const options = {
+  root: null,
+  threshold: 0.3,
+};
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('change');
@@ -21,3 +27,37 @@ navLists.forEach(navList => {
     document.body.classList.remove('overflow');
   });
 });
+
+const featureObserver = new IntersectionObserver(callback, options);
+
+features.forEach(feature => {
+  featureObserver.observe(feature);
+});
+
+document.querySelectorAll('.feature__img').forEach(column => {
+  column.classList.add('hidden-left');
+});
+document.querySelectorAll('.feature__content').forEach(column => {
+  column.classList.add('hidden-right');
+});
+
+function callback(entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  //   get the class name of the target section that came into view
+  const curSectionsName = entry.target.getAttribute('class');
+
+  //   now target that specific section
+  const curSection = document.querySelector(`.${curSectionsName}`);
+  console.log(curSection);
+
+  //   remove the hidden classes (DOM traversing)
+  curSection.firstElementChild.classList.remove('hidden-left');
+  console.log(curSection.firstElementChild);
+  curSection.lastElementChild.classList.remove('hidden-right');
+  console.log(curSection.lastElementChild);
+
+  observer.unobserve(entry.target);
+}
