68d43b6e7a3a27f3c365d9264a2802b3ff92905b
Favourite Jome <54733225+jomefavourite@users.noreply.github.com>
2023-05-07 22:18:31 +0100

    Update auth.js with callback url

--- config/auth.js
+++ config/auth.js
@@ -9,7 +9,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       // callbackURL: "http://localhost:3000/google/callback",
-      callbackURL: "https://plandone-student.herokuapp.com/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       passReqToCallback: true,
     },
     async (request, accessToken, refreshToken, profile, done) => {
